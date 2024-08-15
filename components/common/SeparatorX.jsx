b10569c072ccffaa7a030ee85331828d7f870480
Ethan Arinaitwe <ethanarinaitwe36@gmail.com>
2024-08-15 21:08:45 +0300

    TimeLine Data

---
+++ components/common/SeparatorX.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SeparatorX = () => {
+  return (
+    <div class="line-container">
+      <div class="line"></div>
+    </div>
+  );
+};
+
+export default SeparatorX;
