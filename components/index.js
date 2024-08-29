531abeaf4a4a10ad6537a63ea8463b2beffcc7a3
bronia-linda <broniaaturinda@proton.me>
2024-08-28 18:50:12 -0700

    quick fix

--- components/index.js
+++ components/index.js
@@ -6,6 +6,6 @@ export { default as HeroBanner } from './HeroBanner';
 export { default as VideoCard } from './VideoCard';
 export { default as FooterBanner } from './FooterBanner';
 export { default as TicketBanner } from './TicketBanner';
-export { default as InterBanner } from './InterBanner';
+// export { default as InterBanner } from './InterBanner';
 export { default as ConferencesBanner } from './ConferencesBanner';
 export { default as Cart } from './Cart';
